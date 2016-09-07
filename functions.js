091a135f8025a2882fcfeab71bcf812ef0e1e79f
David Maurer <davemaurermail@gmail.com>
2016-09-06 23:31:05 -0600

    Add comments on different function types.

--- functions.js
+++ functions.js
@@ -1,10 +1,22 @@
 // Functions can be named and unnamed
 
+
+// function expression, alse anonymous: Function name is optional (as in this example without one). The variable is
+// hoisted, it's value is not, so defining after declaration will not work in this case.
 var printThis = function () {
   console.log('This');
 }
 
+// function delcaration: They are hoisted, so the function call can happen
+// before the function is declared. Delcarations cannot be defined inside of
+// conditional statements or loops.
 function printThis () {
   console.log('This');
+}
+
+// named function expression: The function name is not available outside of the function, so the global
+// scope in this case will not have access to foo. This type of function is useful for recursion.
+var func = function foo () {
+  console.log(typeof foo);
 }
 
