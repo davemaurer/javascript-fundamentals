62bd6eabc7a50aca2f7cb05b41d8f8d25883d743
David Maurer <davemaurermail@gmail.com>
2016-09-22 22:29:32 -0600

    Example of directly defining a callback when invoking the higher order function
    it's associated with.

--- callbacks.js
+++ callbacks.js
@@ -19,3 +19,19 @@ console.log(mashTogether('Hello', ' there!', add)); // Gives us => Hello there
 
 // In the example above, the callback argument passed in to mashTogether(1, 2, add);, is the add function. So the
 // add function IS the callback.
+
+// You can use callbacks directly when invoking the higher order function the callback is associated with.
+
+console.log(mashTogether('Hello', ' there!', function(hello, there) {
+  return hello + there + ' You awesome person you!'
+}));
+// Gives us => Hello there! You awesome person you!
+
+/*
+Callbacks can also be part of callback hooks.
+ */
+
+var img = Image.new(blah blah);
+img.onload(function() {
+  Do some stuff to the image
+});
